637971c783353120726e4525512c0a7254fffe88
Pablo Naveira López <pablonaveiralopez@gmail.com>
2024-08-24 12:55:21 +0200

    feat: test husky

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
                   <Link
-                    href="#"
+                    href="https://github.com/anbrela/ultimate-next14-starter-i18n-tailwind-typescript-shadcn"
                     className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
