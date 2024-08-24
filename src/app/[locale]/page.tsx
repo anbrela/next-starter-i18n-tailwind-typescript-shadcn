1bad7f24c0bc070a6fcb55e4559ce0545dcf7163
Pablo Naveira López <pablonaveiralopez@gmail.com>
2024-08-24 13:07:40 +0200

    feat: updated page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -84,9 +84,9 @@ export default function Home() {
                     className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
-                    View Repository
+                    View Repository in githu
                   </Link>
-                  
+
                 </div>
               </div>
             </div>
